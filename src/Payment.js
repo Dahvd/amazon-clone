7b64f792c3829d6d5b89f778f2351a0911ca9327
David Oligney <davidoligney@gmail.com>
2021-09-09 12:07:51 -0600

    payment now have to do firebase stuff which wont work on laptop I guess

--- src/Payment.js
+++ src/Payment.js
@@ -1,22 +1,58 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {styles} from './Payment.css'
 import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from './reducer';
+import axios from './axios';
 
 export default function Payment () {
 
     const [{basket, user}, dispatch] = useStateValue();
+    const history = useHistory();
     
     const stripe = useStripe();
     const elements = useElements();
-
+    
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
     const [error, setError] = useState(null);
     const [disabled, setDisabled] = useState(true);
+    const [clientSecret, setClientSecret] = useState(true);
 
-    const handleSubmit = e => {
+    useEffect(() => {
+        //generate stripe secret which lets you charge a customer
+        const getClientSecret = async () => {
+            const response = await axios({
+                method: 'post',
+                //stripe needs payments in cents so *100
+                url: `/payments/create?total=${getBasketTotal(basket) * 100}`
+            });
+            setClientSecret(response.data.clientSecret)
+        }
+        getClientSecret();
+    }, [basket])
+
+    const handleSubmit = async (event) => {
         //stripe stuff
+        event.preventDefault(); //Stops refreshing
+        setProcessing(true);
+
+        const payload = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement)
+            }
+        }).then(({ paymentIntent }) => {
+            //paymentItent is payment confirmation
+
+            setSucceeded(true);
+            setError(null);
+            setProcessing(false);
+
+            history.replace('/orders');
+        })
     }
     const handleChange = event => {
         //stripe stuff
@@ -73,6 +109,28 @@ export default function Payment () {
                             
                         <form onSubmit={handleSubmit}>
                             <CardElement onChange={handleChange} /> 
+                            <div className='payment_priceCotainer'>
+                            <CurrencyFormat
+                                renderText={(value) => (
+                                <>
+                                    <h3>Order Total: {value}</h3>
+                                </>
+                                )}
+                                decimalScale={2}
+                                value={getBasketTotal(basket)}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                                prefix={'$'}
+                            />
+                            <button disabled={processing || disabled || succeeded}>
+                                <span>
+                                    {processing ? <p>Processing</p> : 'Buy Now'}
+                                </span>
+                            </button>
+                            </div>
+
+                            {/* We have Error state so if error show this div */}
+                            {error && <div>{error}</div>}
                         </form>
                     </div>
                 </div>
